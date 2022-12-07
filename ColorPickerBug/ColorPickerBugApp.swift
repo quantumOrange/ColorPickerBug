30c9603eb2984b654a2f30e4bdd719420480419f
David Crooks <crodave@gmail.com>
2022-12-07 11:32:54 +0100

    Initial Commit

---
+++ ColorPickerBug/ColorPickerBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerBugApp.swift
+//  ColorPickerBug
+//
+//  Created by David Crooks on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
